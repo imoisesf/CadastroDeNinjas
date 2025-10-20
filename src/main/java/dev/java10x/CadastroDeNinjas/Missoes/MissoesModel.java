fd2401ce54c7cc5609f55cd825cea5480c7ed183
imoisesf <imoisesf@emeal.nttdata.com>
2025-10-20 19:36:39 -0300

    feat: Coloca o Lombok na calsse para abstracao

--- src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesModel.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesModel.java
@@ -3,11 +3,17 @@ package dev.java10x.CadastroDeNinjas.Missoes;
 import dev.java10x.CadastroDeNinjas.DificuldadeMissao;
 import dev.java10x.CadastroDeNinjas.Ninjas.NinjaModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Entity
 @Table(name = "tb_missoes")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class MissoesModel {
 
     @Id
@@ -21,36 +27,4 @@ public class MissoesModel {
     @OneToMany(mappedBy = "missoes")
     private List<NinjaModel> ninjas;
 
-    public MissoesModel(String nome, String descricao, DificuldadeMissao dificuldade) {
-        this.nome = nome;
-        this.descricao = descricao;
-        this.dificuldade = dificuldade;
-    }
-
-    public MissoesModel() {
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getDescricao() {
-        return descricao;
-    }
-
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
-    }
-
-    public DificuldadeMissao getDificuldade() {
-        return dificuldade;
-    }
-
-    public void setDificuldade(DificuldadeMissao dificuldade) {
-        this.dificuldade = dificuldade;
-    }
 }
